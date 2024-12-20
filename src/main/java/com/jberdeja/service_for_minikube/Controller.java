a32ce7a34e51675bc33c2526b3d6864891ece4dc
jhonny-berdeja <jhonny.berdeja.100@gmail.com>
2024-12-19 21:11:30 -0300

    first commit

---
+++ src/main/java/com/jberdeja/service_for_minikube/Controller.java
@@ -0,0 +1,22 @@
+package com.jberdeja.service_for_minikube;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/get")
+    public String checkGet() {
+        System.out.println("Execute get");
+        return "get-ok";
+    }
+
+    @PostMapping("/post")
+    public String checkPost(@RequestBody String name) {
+        System.out.println("Execute post");
+        return name + " - ok";
+    }
+}
---
+++ src/main/java/com/jberdeja/service_for_minikube/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.jberdeja.service_for_minikube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
